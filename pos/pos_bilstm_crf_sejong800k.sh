d09bd275e00d6bb298531cf544d41c139b7e8ea8
Jongseong Woo <22februa@gmail.com>
2019-01-17 14:42:28 +0900

    modified pos_bilstm_crf_sejong800k.sh

--- pos/pos_bilstm_crf_sejong800k.sh
+++ pos/pos_bilstm_crf_sejong800k.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+INPUT_DIR=/data/pos_sejong800k/conll
+OUTPUT_DIR=/data/bilstm_crf_train/pos_bilstm_crf_sejong800k
+NECESSARY_FILE=/data/bilstm_crf_train/pos_bilstm_crf_sejong800k/vocab.pkl
+TRAIN_LINES=50
+
 export CUDA_VISIBLE_DEVICES=2
 
-python -m main
+python -m main \
+  --input_dir=$INPUT_DIR \
+  --output_dir=$OUTPUT_DIR \
+  --necessary_file=$NECESSARY_FILE \
+  --train_lines=$TRAIN_LINES
